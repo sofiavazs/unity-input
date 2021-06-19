999491bf6df1bca84f84c8f56299f6a62a4eb97c
Sofia Sousa <sofiavazsousa@gmail.com>
2021-06-19 19:15:16 +0200

    add my changes

---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 3.5f;
+	public float jumpingForce = 10f;
+	public float rotatingSpeed = 40f;
+
+	private bool canJump = false;
+    // Start is called before the first frame update
+    void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+			if (Input.GetKey("right")) {
+				transform.RotateAround(transform.position, Vector3.up, rotatingSpeed * Time.deltaTime);
+			}
+			if (Input.GetKey("left")) {
+				transform.RotateAround(transform.position, Vector3.up, -rotatingSpeed * Time.deltaTime);
+			}
+			if (Input.GetKey ("up")) {
+				transform.position += transform.forward * speed * Time.deltaTime;
+			}
+			if (Input.GetKeyDown("space") && canJump) {
+				canJump = false;
+				GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
+			}
+    }
+		
+		void OnCollisionEnter (Collision collision) {
+			if (collision.transform.name == "Floor") {
+				canJump = true;
+			}
+		}
+}
